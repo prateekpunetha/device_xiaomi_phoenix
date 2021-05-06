6f127852eb2fb8e1342f2b36aa65694efb0f1248
prateekpunetha <hi@prateekpunetha.dev>
2021-05-06 10:11:45 +0530

    phoenix: libinit: Update Sunfish fp to RQ2A.210505.002
    Signed-off-by: prateekpunetha <hi@prateekpunetha.dev>

--- init/init_phoenix.cpp
+++ init/init_phoenix.cpp
@@ -124,7 +124,7 @@ void vendor_load_properties() {
     region = GetProperty("ro.boot.hwc", "");
 
     if (region == "CN") {
-        char const fp[] = "google/sunfish/sunfish:11/RQ2A.210405.005/7181113:user/release-keys";
+        char const fp[] = "google/sunfish/sunfish:11/RQ2A.210505.002/7246365:user/release-keys";
 
         for (const auto &source : ro_props_default_source_order) {
 
@@ -138,7 +138,7 @@ void vendor_load_properties() {
         }
         property_override("ro.build.description", "phoenix-user 10 QKQ1.190825.002 V11.0.9.0.QGHCNXM release-keys");
     } else if (region == "INDIA") {
-       char const fp[] = "google/sunfish/sunfish:11/RQ2A.210405.005/7181113:user/release-keys";
+       char const fp[] = "google/sunfish/sunfish:11/RQ2A.210505.002/7246365:user/release-keys";
 
         for (const auto &source : ro_props_default_source_order) {
 
